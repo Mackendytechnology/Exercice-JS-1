ae634e08c1ad5fad3a74fb5b47079a9e7b2f58ed
MackendyTechnology <bouquetmackendy34@gmail.com>
2023-10-09 17:00:47 -0400

    Exercice-JS-1

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const newString = 'Mackendy';
+console.log(newString);
+
+
+const newNumero = 25;
+console.log(newNumero);
+
+//Boolean 1
+const newBoolean = true;
+console.log(newBoolean);
+
+//Boolean 2
+const a = true;
+const b = false;
+
+const resultAnd = a && b;
+console.log(resultAnd);
+
+const resultOr = a || b;
+console.log(resultOr);
+
+const resultNot = !a;
+console.log(resultNot);
+
+const newRest = 10 - 7 === 3;
+console.log(newRest);
+
+
+const multiply = 10 * 4 === 40;
+console.log(multiply);
+
+
+const newModule = 21 % 5 === 1;
+console.log(newModule);
